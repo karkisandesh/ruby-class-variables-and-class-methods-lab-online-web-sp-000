5d8819d96622529823b03c1e3a6486ac31a21245
Sandesh Karki <karkisandesh@hotmail.com>
2019-02-04 18:33:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   
   @@count = 0
   @@artists = [ ]
+  @@genres = [ ]
   
   
   attr_accessor :name, :artist, :genre
@@ -12,6 +13,8 @@ class Song
     @genre = genre
     @@count += 1
     @@artists << artist
+    @@genres << genre 
+  
   end
   
   def count 
